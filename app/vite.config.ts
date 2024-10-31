7d0b3ef8dc1cad087b6e618ef0cff27ea9652f35
Sara DeLessio <sara_delessio@abtassoc.com>
2024-10-31 15:54:50 -0400

    Update vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    base: '/capd-test-docs/', 
-    server: {
-        open: true 
-    },
-});
+  plugins: [react()],
+  base: "/capd-test-pages/"
+})
