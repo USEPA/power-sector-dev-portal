3751a344e9ea10ab7bd166c102f0468c0604f9ae
Sara DeLessio <sara_delessio@abtassoc.com>
2024-10-31 15:49:32 -0400

    Update base url and homepage url

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/capd-test-docs/app/', 
+    base: '/capd-test-docs/', 
     server: {
         open: true 
     },
