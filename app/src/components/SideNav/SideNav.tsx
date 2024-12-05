8af2da440cb3e6fee9bc12a7c062da3abaf55b20
Sara DeLessio <sara_delessio@abtassoc.com>
2024-12-05 14:41:48 -0500

    Create sidenav component

---
+++ app/src/components/SideNav/SideNav.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from "react";
+import USWDS from "@uswds/uswds/js";
+const { inPageNavigation } = USWDS;
+import "./SideNav.scss";
+
+const SideNav: React.FC = () => {
+  const sideNavRef = useRef<HTMLElement | null>(null);
+  const initialized = useRef(false); 
+
+  useEffect(() => {
+    if (!initialized.current) {
+      const timer = setTimeout(() => {
+        if (sideNavRef.current) {
+          inPageNavigation.on(sideNavRef.current);
+          initialized.current = true; 
+        }
+      }, 100);
+
+      // Cleanup
+      return () => {
+        if (sideNavRef.current) {
+          inPageNavigation.off();
+        }
+        clearTimeout(timer);
+      };
+    }
+  }, []); 
+
+  return (
+    <aside
+      id="sidenav"
+      className="usa-in-page-nav"
+      data-title-text="On this page"
+      data-title-heading-level="h4"
+      data-scroll-offset="0"
+      data-root-margin="0px 0px 0px 0px"
+      data-threshold="1"
+      ref={sideNavRef}
+    >
+    </aside>
+  );
+};
+
+export default SideNav;
