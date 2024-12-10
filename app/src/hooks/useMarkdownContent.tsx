11921e95d61c3867e9259c58693d6392dd56776a
Sara DeLessio <sara_delessio@abtassoc.com>
2024-12-10 13:16:19 -0500

    comment out gray matter

--- app/src/hooks/useMarkdownContent.tsx
+++ app/src/hooks/useMarkdownContent.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import matter from 'gray-matter';
+// import matter from 'gray-matter';
 
 const useMarkdownContent = (path: string) => {
   const [content, setContent] = useState<string>('');
