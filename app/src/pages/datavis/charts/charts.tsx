f15ec63c61324f8223459f23c39c5c2def28cf4a
Sara DeLessio <sara_delessio@abtassoc.com>
2024-12-17 11:51:04 -0500

    Switch to use mobile images on chart page

--- app/src/pages/datavis/charts/charts.tsx
+++ app/src/pages/datavis/charts/charts.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import useMarkdownContent from '../../../hooks/useMarkdownContent';
-import Banner from '../../../components/Banner/Banner';
-import { extractBanner, extractIntro, extractSections } from '../../../utilities/extractContent';
-import { Section, Card } from '../../../types/ContentTypes';
-import './Charts.scss';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { Link } from 'react-router-dom';
+import React from "react";
+import useMarkdownContent from "../../../hooks/useMarkdownContent";
+import Banner from "../../../components/Banner/Banner";
+import {
+  extractBanner,
+  extractIntro,
+  extractSections,
+} from "../../../utilities/extractContent";
+import { Section, Card } from "../../../types/ContentTypes";
+import "./Charts.scss";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Link } from "react-router-dom";
+import useIsMobile from "../../../hooks/useIsMobile";
 
 const Charts: React.FC = () => {
   const base = import.meta.env.BASE_URL;
-  const { content, error } = useMarkdownContent(`${base}content/datavis/charts/charts.md`);
+  const { content, error } = useMarkdownContent(
+    `${base}content/datavis/charts/charts.md`
+  );
+  const isMobileView = useIsMobile();
 
-  const { title, tagline } = content ? extractBanner(content) : { title: '', tagline: '' };
-  const { introTitle, introContent } = content ? extractIntro(content) : { introTitle: '', introContent: '' };
+  const { title, tagline } = content
+    ? extractBanner(content)
+    : { title: "", tagline: "" };
+  const { introTitle, introContent } = content
+    ? extractIntro(content)
+    : { introTitle: "", introContent: "" };
   const sections: Section[] = content ? extractSections(content) : [];
 
   if (error) {
@@ -31,18 +43,27 @@ const Charts: React.FC = () => {
           <div key={index} className="section">
             <h3>{section.title}</h3>
             <div className="cards">
-              {section.cards && section.cards.map((card: Card, idx: number) => (
-                <div key={idx} className="card">
-                  <img src={card.image} alt={card.title} />
-                  <div className="card-content">
-                  <h4>{card.title}</h4>
-                  <p>{card.content}</p>
-                  {card.link ?
-                  <Link to={card.link}>Learn about {card.title} <ArrowForwardIcon /></Link> 
-                  : " " }
+              {section.cards &&
+                section.cards.map((card: Card, idx: number) => (
+                  <div key={idx} className="card">
+                    {isMobileView ? (
+                      <img src={card.imagemb} alt={card.title} />
+                    ) : (
+                      <img src={card.image} alt={card.title} />
+                    )}
+                    <div className="card-content">
+                      <h4>{card.title}</h4>
+                      <p>{card.content}</p>
+                      {card.link ? (
+                        <Link to={card.link}>
+                          Learn about {card.title} <ArrowForwardIcon />
+                        </Link>
+                      ) : (
+                        " "
+                      )}
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         ))}
--- app/src/types/ContentTypes.tsx
+++ app/src/types/ContentTypes.tsx
@@ -3,6 +3,7 @@ export interface Card {
   content?: string;
   link?: string;
   image: string;
+  imagemb?: string;
   type: 'do' | 'do-not'; 
 }
 
@@ -11,6 +12,7 @@ export interface Section {
   title?: string;
   content?: string;
   image?: string;
+  imagemb?: string;
   level?: number;
   cards?: Card[]; 
 }
