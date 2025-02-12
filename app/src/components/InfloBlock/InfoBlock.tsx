d82145496960441ce476346b9d921aa15e323fb5
Sara DeLessio <sara_delessio@abtassoc.com>
2025-02-12 16:36:59 -0500

    Update external links on cards

--- app/src/components/InfloBlock/InfoBlock.tsx
+++ app/src/components/InfloBlock/InfoBlock.tsx
@@ -1,28 +1,38 @@
 // src/components/datavis/InfloBlock/InfoBlock.tsx
-import React from 'react';
-import './InfoBlock.scss';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./InfoBlock.scss";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Link } from "react-router-dom";
 
 interface InfoBlockProps {
-    title?: string;
-    content?: string;
-    link?: string; 
-    type?: string;
+  title?: string;
+  content?: string;
+  link?: string;
+  type?: string;
 }
 
-const InfoBlock: React.FC<InfoBlockProps> = ({ title, content, link, type }) => {
-    return (
-        <div className="info-block">
-            <h3>{title}</h3>
-            {content && <p>{content}</p>}
-            {link && (
-                <Link to={link}>
-                    {type === "visit" ? 'Visit' : 'Learn about'} {title} <ArrowForwardIcon />
-                </Link>
-            )}
-        </div>
-    );
+const InfoBlock: React.FC<InfoBlockProps> = ({
+  title,
+  content,
+  link,
+  type,
+}) => {
+  return (
+    <div className="info-block">
+      <h3>{title}</h3>
+      {content && <p>{content}</p>}
+      {link &&
+        (type === "visit" ? (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            Visit {title} <ArrowForwardIcon />
+          </a>
+        ) : (
+          <Link to={link}>
+            Learn about {title} <ArrowForwardIcon />
+          </Link>
+        ))}
+    </div>
+  );
 };
 
 export default InfoBlock;
