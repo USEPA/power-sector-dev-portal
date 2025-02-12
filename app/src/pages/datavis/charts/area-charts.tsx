a2d23d5cdcde2376fa0c417b3c69cd58b4588572
Sara DeLessio <sara_delessio@abtassoc.com>
2025-02-12 15:52:28 -0500

    Add content to area charts page

--- app/src/pages/datavis/charts/area-charts.tsx
+++ app/src/pages/datavis/charts/area-charts.tsx
@@ -14,11 +14,12 @@ import SideNav from "../../../components/SideNav/SideNav";
 
 const AreaCharts: React.FC = () => {
   const base = import.meta.env.BASE_URL;
-  const { content, error } = useMarkdownContent(`${base}content/datavis/charts/area-charts.md`);
+  const { content, error } = useMarkdownContent(`${base}content/datavis/charts/area-charts.md`
+  );
   const { title } = content ? extractBanner(content) : { title: "" };
   const { introTitle, introContent } = content
     ? extractIntro(content)
-    : { introTitle: "", introContent: "" }; 
+    : { introTitle: "", introContent: "" };
 
   const sections: Section[] = content ? extractSections(content) : [];
 
@@ -27,19 +28,17 @@ const AreaCharts: React.FC = () => {
   }
 
   return (
-    <div className="color-page">
-     <div className="usa-in-page-nav-container container-with-sidenav">
-     <main id="main-content" className="main-content">
-      <div className="intro-section">
-        <h1>{title}</h1>
-        <h2>{introTitle}</h2>
-        {introContent && (
-          <ReactMarkdown>{introContent}</ReactMarkdown>
-        )}
+    <div className="area-charts-page">
+         <div className="usa-in-page-nav-container container-with-sidenav">
+         <main id="main-content" className="main-content">
+        <div className="intro-section">
+          <h1>{title}</h1>
+          <h2>{introTitle}</h2>
+          {introContent && <ReactMarkdown>{introContent}</ReactMarkdown>}
         </div>
 
-        {sections.map((section) => (
-          <>
+        {sections.map((section, idx) => (
+          <div key={idx}>
             {section.title && renderHeader(section.title, section.level || 3)}
             {section.content && (
               <div>
@@ -70,7 +69,7 @@ const AreaCharts: React.FC = () => {
                 ))}
               </div>
             )}
-          </>
+          </div>
         ))}
         </main>
         <SideNav />
