93be0efe83739c2ae05f874ba2230250d6b41cb1
Sara DeLessio <sara_delessio@abtassoc.com>
2025-02-13 16:50:04 -0500

    Add color coded lines for open source cards

--- app/src/components/Card/Card.tsx
+++ app/src/components/Card/Card.tsx
@@ -1,30 +1,59 @@
-import React from 'react';
-import './Card.scss';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Card.scss";
+import { Link, useLocation } from "react-router-dom";
 
 interface CardProps {
-    title?: string;
-    content?: string;
-    link?: string; 
+  title?: string;
+  content?: string;
+  link?: string;
 }
 
+const getClassNameFromTitle = (title?: string): string => {
+  if (!title) return "";
+
+  const allowedTitles = [
+    "Data Visualization Style Guide",
+    "R Style Guide",
+    "API",
+  ];
+
+  if (allowedTitles.includes(title)) {
+    return title
+      .split(" ") 
+      .map((word, index) =>
+        index === 0
+          ? word.toLowerCase()
+          : word.charAt(0).toUpperCase() + word.slice(1)
+      )
+      .join(""); 
+  }
+
+  return "";
+};
+
 const Card: React.FC<CardProps> = ({ title, content, link }) => {
-    return (
-        <div className="card">
-            <h3>{title}</h3>
-            {content && <p className="content">{content}</p>}
-            {link && (
-                <Link to={link} className="usa-button blue-button">
-                    {title === "Principles" ? "Read the Principles" 
-                    : title === "Design Elements" ? "Learn About Design Elements" 
-                    : title === "Charts" ? "Choose a Chart"
-                    : title === "Further Readings" ? "Find Further Readings"
-                    : `Go to ${title}`
-                    }
-                </Link>
-            )}
-        </div>
-    );
+  const { pathname } = useLocation(); 
+  const className = pathname === "/" ? getClassNameFromTitle(title) : "";
+
+  return (
+    <div className={`card ${className}`}>
+      <h3>{title}</h3>
+      {content && <p className="content">{content}</p>}
+      {link && (
+        <Link to={link} className="usa-button blue-button">
+          {title === "Principles"
+            ? "Read the Principles"
+            : title === "Design Elements"
+            ? "Learn About Design Elements"
+            : title === "Charts"
+            ? "Choose a Chart"
+            : title === "Further Readings"
+            ? "Find Further Readings"
+            : `Go to ${title}`}
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Card;
