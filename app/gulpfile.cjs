bd99e88738164cfaa8aefde1bb036507a3887109
Sara DeLessio <sara_delessio@abtassoc.com>
2025-03-18 12:48:59 -0400

    update gulpfile to reference public url

--- app/gulpfile.cjs
+++ app/gulpfile.cjs
@@ -1,6 +1,6 @@
-// gulpfile.js
-
-const uswds = require("@uswds/compile");
+const gulp = require('gulp');
+const replace = require('gulp-replace'); 
+const uswds = require('@uswds/compile');
 
 // USWDS version
 uswds.settings.version = 3;
@@ -8,8 +8,16 @@ uswds.settings.version = 3;
 // Path settings
 uswds.paths.dist.css = './src/assets/css';
 uswds.paths.dist.theme = './src/styles/uswds';
+uswds.paths.dist.fonts = './dist/fonts'; 
+
+
+gulp.task('adjust-font-paths', function () {
+  return gulp.src('./src/assets/css/uswds.min.css') 
+    .pipe(replace(/url\('\/fonts/g, "url('/power-sector-dev-portal/fonts")) 
+    .pipe(gulp.dest('./dist/css')); 
+});
 
 // Export tasks
 exports.init = uswds.init;
-exports.compile = uswds.compile;
+exports.compile = gulp.series(uswds.compile, 'adjust-font-paths'); 
 exports.watch = uswds.watch;
