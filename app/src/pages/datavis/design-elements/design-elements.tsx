1a4e962e31f19da4384dcb8baee62b6db9d61f9d
Sara DeLessio <sara_delessio@abtassoc.com>
2025-04-02 11:11:57 -0400

    Replace design elements infoblock with charts card

--- app/src/pages/datavis/design-elements/design-elements.tsx
+++ app/src/pages/datavis/design-elements/design-elements.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import useMarkdownContent from "../../../hooks/useMarkdownContent";
 import Banner from "../../../components/Banner/Banner";
-import InfoBlock from "../../../components/InfloBlock/InfoBlock";
-import { extractBanner, extractIntro, extractSections } from "../../../utilities/extractContent";
-import { Section } from "../../../types/ContentTypes"; 
+import {
+  extractBanner,
+  extractIntro,
+  extractSections,
+} from "../../../utilities/extractContent";
+import { Section } from "../../../types/ContentTypes";
+import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
+import useIsMobile from "../../../hooks/useIsMobile";
 
 const DesignElements: React.FC = () => {
+  const isMobileView = useIsMobile();
   const base = import.meta.env.BASE_URL;
-  const { content, error } = useMarkdownContent(`${base}content/datavis/design-elements/design-elements.md`);
+  const { content, error } = useMarkdownContent(
+    `${base}content/datavis/design-elements/design-elements.md`
+  );
 
   // Extract content from markdown
-  const { title, tagline } = content ? extractBanner(content) : { title: '', tagline: '' };
-  const { introTitle, introContent } = content ? extractIntro(content) : { introTitle: '', introContent: '' };
+  const { title, tagline } = content
+    ? extractBanner(content)
+    : { title: "", tagline: "" };
+  const { introTitle, introContent } = content
+    ? extractIntro(content)
+    : { introTitle: "", introContent: "" };
   const sections: Section[] = content ? extractSections(content) : [];
 
   if (error) {
@@ -27,15 +40,28 @@ const DesignElements: React.FC = () => {
           <p>{introContent}</p>
         </div>
         {sections.map((section, idx) => (
-          <div key={idx} className="grid grid-col--one">
-            {section.cards && section.cards.map((card, cardIdx) => (
-              <InfoBlock
-                key={cardIdx}
-                title={card.title}
-                content={card.content}
-                link={card.link}
-              />
-            ))}
+          <div key={idx} className="section">
+            {section.cards &&
+              section.cards.map((card) => (
+                <div className="charts-card">
+                  {isMobileView ? (
+                    <img src={card.imagemb} alt={card.alt} />
+                  ) : (
+                    <img src={card.image} alt={card.alt} />
+                  )}
+                  <div className="card-content">
+                    <h4>{card.title}</h4>
+                    {card.content && (
+                      <ReactMarkdown>{card.content}</ReactMarkdown>
+                    )}
+                    {card.link && (
+                      <Link className="usa-button blue-button" to={card.link}>
+                        Learn about {card.title}
+                      </Link>
+                    )}
+                  </div>
+                </div>
+              ))}
           </div>
         ))}
       </div>
